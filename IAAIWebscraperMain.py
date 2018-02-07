d1e8834dc9af3cd3cdd04cf329a31743b335f3ad
Ion298 <36217512+Ion298@users.noreply.github.com>
2018-02-06 22:53:23 -0600

    Add files via upload

--- IAAIWebscraperMain.py
+++ IAAIWebscraperMain.py
@@ -9,31 +9,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 
+##Starts Selenium maximized and renames it to the variable 'driver'
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("load-extension=C:\\Users\\Zach\\Downloads\\cjpalhdlnbpafiamejdnhcphjbkeiagm\\1.14.18_0")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.create_options()
 
+##Defines how many iterations do run main
 def iterationNumber():
     iterations = input('How many cars are you saving? ')
     return iterations
 
+##Module to restart the page if it times out while loading
 def loadPage( str ):
     driver.set_page_load_timeout(10)
     try: 
         driver.get( str ) 
-        return False
+        return True
     except TimeoutException:
         driver.execute_script("window.stop();")
-        return True
+        return False
 
+##Calls the loadPage method on the IAAI login website and logs in, USER MUST INPUT CREDENTIALS IN THE CODE!
 def loginIAAI():
     page = ('https://www.iaai.com/Login/LoginPage?ReturnUrl=/MyDashboard/Default')
-    pageLoader = True
-    while pageLoader & boolean:
+    
+    ##Runs the pageloader until the page successfully loads
+    pageLoader = False
+    while not pageLoader:
         pageLoader = loadPage(str = page)
      
+    ##Waits for the usrname field to become visible and then inputs usrname&pswrd and submits
     WebDriverWait(driver, 100).until( lambda driver: driver.find_element_by_name("usrName"))
     
     usernameField = driver.find_element_by_name("usrName")
@@ -44,9 +50,9 @@ def loginIAAI():
     pswdField.click()
     pswdField.send_keys("PUT YOUR PASSWORD HERE")
     pswdField.submit()
-     
-    dropDownMenu = driver.find_element_by_xpath("//nav[@id='mainNav']//a[contains(@href,'MyDashboard')]")
     
+    ##Waits for the required drop down menu to load, hovers over it, and clicks the 'purchase history' icon 
+    dropDownMenu = driver.find_element_by_xpath("//nav[@id='mainNav']//a[contains(@href,'MyDashboard')]")
     WebDriverWait(driver, 100).until( lambda driver: dropDownMenu )
     
     driver.move_to_element(dropDownMenu)
