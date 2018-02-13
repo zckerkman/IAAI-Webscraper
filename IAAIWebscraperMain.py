a0b90a28aebc6ecf27f4780a8c4294239d98bfa7
Zach Kerkman <zckerkman@hotmail.com>
2018-02-12 23:25:29 -0600

    Updated Main, still waiting for client to retrieve price elements

--- IAAIWebscraperMain.py
+++ IAAIWebscraperMain.py
@@ -12,12 +12,11 @@ from selenium.webdriver.chrome.options import Options
 
 import pyautogui
 
-
 from openpyxl import load_workbook
 from openpyxl.styles import Font
 
 import os
-import time
+import time 
 
 ##defines current folder, used to save car images before moving them into new folders
 dirPath = os.path.dirname(os.path.realpath(__file__))
@@ -76,8 +75,8 @@ def createFolder(str):
     os.makedirs(os.path.join(dirPath,str))
     
 def carInformation( int ):
-    ##finds the price of the car 
-    
+    ##finds the price of the car
+    carPrice = driver.find_element_by_xpath('//a[@class=])    
     
     ##clickes the car we want info on with respect to the iteration number
     driver.find_element_by_xpath('//a[@class id="stockitemID_%s"]/a[conatins(@href onclick,"return")]' % int).click
@@ -87,7 +86,7 @@ def carInformation( int ):
     vinNumber = driver.find_element_by_xpath('//div[contains(@class,"vehicle-info-wrapper")]/span[contains(@id,"VIN_vehicleStats")]').text
     
     carModel = driver.find_element_by_xpath('//div[@class="flex-item"]/h1[@class="pd-title-ymm"]').text
-
+    
     carColors = driver.find_element_by_xpath('//div[@class="tabs tab-vehicle waypoint-trigger"]/div[@class="row flex"][14]/div[contains(@class,"flex-self-end")]')
     carColors.location_once_scrolled_into_view
     ##splits the exterior and interior colors
@@ -97,19 +96,29 @@ def carInformation( int ):
     ##removes the text after the mileage of the car
     carMileageArray = carMileage.text.split("mi")
     
+    ##concatenates all the parts of the typeOfVehicle
     typeOfVehicle = (carModel + " " + carMileageArray.pop[0] + " " + carColorsArray.pop[0] + " " + vinNumber)
-    
-    createFolder(str=typeOfVehicle)
+    ##removes the spaces
+    vehicleSaveName = typeOfVehicle.replace(" ","")
+    ##creates a folder for the vehicle type
+    createFolder(str=vehicleSaveName)
     
     driver.find_element_by_xpath('//a[@id="DownloadImages"]').click
-    imageFolderName = typeOfVehicle + " Images.ZIP"
+    imageFolderName = vehicleSaveName + "Images.ZIP"
     pyautogui.typewrite(imageFolderName)
     pyautogui.press('enter')
-    os.rename(os.path.join(dirPath,imageFolderName),os.path.join(dirPath,typeOfVehicle,imageFolderName))
+    
+    ##Defines the initial folder path and the final folder path for the images
+    folderInitialPath = os.path.join(dirPath,imageFolderName)
+    folderFinalPath = os.path.join(dirPath,vehicleSaveName,imageFolderName)
+   
+    ##checks to see if the image folder has been downloaded, if it has it adds it to the folder for the vehicle
+    while not os.path.exists(folderInitialPath):
+        time.sleep(1)
+    os.rename(folderInitialPath,folderFinalPath)
     
     ##finds the damage on the vehicle
-    typeOfDamage = driver.find_element_by_xpath('//div[@class="pd-condition-wrapper]/div[@flass="row flex"]/div[contains(@class,"flex-self-end")]').text
-                                           
+    typeOfDamage = driver.find_element_by_xpath('//div[@class="pd-condition-wrapper]/div[@flass="row flex"]/div[contains(@class,"flex-self-end")]').text                                  
                                                
     carInformationArray = [typeOfDamage,price,typeOfVehicle]
     
@@ -117,15 +126,18 @@ def carInformation( int ):
    
 
 def addToExcelSpreadsheet ( str, list ):    
+    
     ##loads the template workbook
     templateExcelSpreadsheet = "Cost_of_Vehicle_Format_Master.xlsx"
     templateWorkbook = load_workbook(templateExcelSpreadsheet)
     
+    #sets the font to change the styles in A1 and A2 to bold and italics
+    font = Font(name = 'Arial', size = 9, bold = True, italic = True)
+    
     ##adds the price of the car
     templateWorkbook['B5'] = list.pop
     
-    ##adds the mileage, color, car model, and vin to the Type of Vehicle after bolding and italicizing it
-    font = Font(name = 'Arial', size = 9, bold = True, italic = True)
+    ##adds the carType and the text already in the template workbook
     initialA1Text = templateWorkbook['A1']
     templateWorkbook['A1'] = initialA1Text + str
       
@@ -133,24 +145,35 @@ def addToExcelSpreadsheet ( str, list ):
     initialA2Text = templateWorkbook['A2']
     templateWorkbook['A2'] = initialA2Text + list.pop
     
+    ##bolds and italicizes the carType and damage
+    templateWorkbook['A1'] = font
+    templateWorkbook['A2'] = font
     
     ##saves the excel file as the Type of Vehicle in the folder with the images 
-    fileName = str + ".xlsx"
-    templateWorkbook.save(os.path.join(dirPath,str,fileName, as_template=False))
+    vehicleSaveName = str.remove(" ","")
+    fileName = vehicleSaveName + "Spreadsheet.xlsx"
+    templateWorkbook.save(os.path.join(dirPath,vehicleSaveName,fileName), as_template=False))
     
 def main():
+    ##returns the interations function
     iterations = iterationNumber()
+    ##logs in to the website
     loginIAAI()
+    ##defines the array for typeOfVehicle keys
     vehicles = []
+    ##defines the dictionary to hold the price and damage type
     carDictionary = {}
     for x in range (0, iterations):
+        ##navigates to recent purchases
         returnToPurchases()
         carInformationArray = carInformation( int = x )
+        ##adds the typeOfVehicle to vehciles and sets it as a the key with price and damage as valuse
         typeOfVehicle = carInformationArray.pop
         vehicles.append(typeOfVehicle)
         carDictionary[typeOfVehicle] = carInformationArray
 
     for cars in vehicles:
+        ##adds each dictionary key and value to a spreadsheet
         addToExcelSpreadsheet(str = cars, list = carDictionary[cars])
         
 if __name__ == "__main__":
